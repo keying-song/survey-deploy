ed5735b8c0e792319573890e67239bb6cc7ce074
KS <102123559+keying-song@users.noreply.github.com>
2023-01-17 18:29:53 -0500

    Update auth-api.js

--- client/src/service/auth-api.js
+++ client/src/service/auth-api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const URL = 'https://centennialsurveygenerator.herokuapp.com/api';
+const URL = 'https://surveygenerator-keying.herokuapp.com//api';
 
 export const userRegister = async(inputs)=>{
     try{
