75ba2c78c82b2150448c674d48d0feb7037dc690
KS <102123559+keying-song@users.noreply.github.com>
2023-01-17 18:13:51 -0500

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const Connection = async(url)=>{
-    const URL ='mongodb+srv://sk1513:j02K8L6uRD4tTNmP@cluster0.eeyo9uy.mongodb.net/?retryWrites=true&w=majority';
+    const URL = "mongodb+srv://sk1513:j02K8L6uRD4tTNmP@cluster0.vi1vlrf.mongodb.net/?retryWrites=true&w=majority";
     try{
        await mongoose.connect(URL, {
             useNewUrlParser:true,
